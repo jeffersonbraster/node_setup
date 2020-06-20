a1f76c84db33c14870c32a5bf96d8fe64918bf0a
Jefferson <jeffersonbraster@gmail.com>
2020-06-19 21:50:27 -0300

    Add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import express from 'express';
+import express from 'express'
 
-
-const app = express();
-
+const app = express()
 
 app.get('/', (request, response) => {
-  return response.json({message: 'oie'});
+  return response.json({ message: 'oie' })
 })
 
-
-app.listen(3333);
+app.listen(3333)
